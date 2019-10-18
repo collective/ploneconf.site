ab6354b25f481be6a45fe3fc4da617eb8b160c08
Philip Bauer <bauer@starzel.de>
2019-10-18 16:09:30 +0200

    create fresh package with bobtemplates.plone

---
+++ src/ploneconf/site/testing.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import ploneconf.site
+
+
+class PloneconfSiteLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        import plone.restapi
+        self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=ploneconf.site)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ploneconf.site:default')
+
+
+PLONECONF_SITE_FIXTURE = PloneconfSiteLayer()
+
+
+PLONECONF_SITE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONECONF_SITE_FIXTURE,),
+    name='PloneconfSiteLayer:IntegrationTesting',
+)
+
+
+PLONECONF_SITE_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(PLONECONF_SITE_FIXTURE,),
+    name='PloneconfSiteLayer:FunctionalTesting',
+)
+
+
+PLONECONF_SITE_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        PLONECONF_SITE_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='PloneconfSiteLayer:AcceptanceTesting',
+)
---
+++ src/ploneconf/site/tests/test_setup.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from ploneconf.site.testing import PLONECONF_SITE_INTEGRATION_TESTING  # noqa: E501
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
+import unittest
+
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class TestSetup(unittest.TestCase):
+    """Test that ploneconf.site is properly installed."""
+
+    layer = PLONECONF_SITE_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if ploneconf.site is installed."""
+        self.assertTrue(self.installer.isProductInstalled(
+            'ploneconf.site'))
+
+    def test_browserlayer(self):
+        """Test that IPloneconfSiteLayer is registered."""
+        from ploneconf.site.interfaces import (
+            IPloneconfSiteLayer)
+        from plone.browserlayer import utils
+        self.assertIn(
+            IPloneconfSiteLayer,
+            utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = PLONECONF_SITE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstallProducts(['ploneconf.site'])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if ploneconf.site is cleanly uninstalled."""
+        self.assertFalse(self.installer.isProductInstalled(
+            'ploneconf.site'))
+
+    def test_browserlayer_removed(self):
+        """Test that IPloneconfSiteLayer is removed."""
+        from ploneconf.site.interfaces import \
+            IPloneconfSiteLayer
+        from plone.browserlayer import utils
+        self.assertNotIn(
+            IPloneconfSiteLayer,
+            utils.registered_layers())
