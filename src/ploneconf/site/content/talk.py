fa84ae2d06f7c839153593c9e1d76303abd0c459
Philip Bauer <bauer@starzel.de>
2020-02-15 12:28:41 +0100

    fix module. add missing implementer and remove missing view from fti

--- src/ploneconf/site/content/talk.py
+++ src/ploneconf/site/content/talk.py
@@ -9,6 +9,7 @@ from ploneconf.site import _
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
+from zope.interface import implementer
 
 
 class ITalk(model.Schema):
@@ -80,6 +81,8 @@ class ITalk(model.Schema):
         required=False,
         )
 
+
+@implementer(ITalk)
 class Talk(Container):
-    """Talk instance
+    """Talk instance class
     """
