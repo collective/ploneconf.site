e339188363af825c54654ac4d72009d3560cc3a9
Philip Bauer <bauer@starzel.de>
2020-04-22 14:20:59 +0200

    add post_handler to setup site

--- src/ploneconf/site/setuphandlers.py
+++ src/ploneconf/site/setuphandlers.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+from plone import api
+from Products.CMFPlone.interfaces import constrains
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+
+import logging
+
+logger = logging.getLogger(__name__)
+PROFILE_ID = 'profile-ploneconf.site:default'
 
 
 @implementer(INonInstallable)
@@ -16,6 +23,72 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    portal = api.portal.get()
+    set_up_content(portal)
+
+
+def set_up_content(portal):
+    """Create and configure some initial content.
+    Part of this code is taken from upgrades.py
+    """
+    # Create the expected folder-structure
+    if 'training' not in portal:
+        training_folder = api.content.create(
+            container=portal,
+            type='Folder',
+            id='training',
+            title=u'Training')
+    else:
+        training_folder = portal['training']
+
+    if 'schedule' not in portal:
+        schedule_folder = api.content.create(
+            container=portal,
+            type='Folder',
+            id='schedule',
+            title=u'Schedule')
+    else:
+        schedule_folder = portal['schedule']
+
+    if 'location' not in portal:
+        location_folder = api.content.create(
+            container=portal,
+            type='Folder',
+            id='location',
+            title=u'Location')
+    else:
+        location_folder = portal['location']
+
+    if 'sponsors' not in portal:
+        sponsors_folder = api.content.create(
+            container=portal,
+            type='Folder',
+            id='sponsors',
+            title=u'Sponsors')
+    else:
+        sponsors_folder = portal['sponsors']
+
+    if 'sprint' not in portal:
+        sprint_folder = api.content.create(
+            container=portal,
+            type='Folder',
+            id='sprint',
+            title=u'Sprint')
+    else:
+        sprint_folder = portal['sprint']
+
+    # Allow logged-in users to create content
+    api.group.grant_roles(
+        groupname='AuthenticatedUsers',
+        roles=['Contributor'],
+        obj=schedule_folder)
+
+    # Constrain addable types to talk
+    behavior = constrains.ISelectableConstrainTypes(schedule_folder)
+    behavior.setConstrainTypesMode(constrains.ENABLED)
+    behavior.setLocallyAllowedTypes(['talk'])
+    behavior.setImmediatelyAddableTypes(['talk'])
+    logger.info('Added and configured {0}'.format(schedule_folder.absolute_url()))
 
 
 def uninstall(context):
