fba0bbe9a22db93c1d631e0449d4561d3e67c3cc
Katja Suess <k.suess@rohberg.ch>
2020-04-24 19:48:19 +0200

    dependencies membrane

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
         'plone.restapi',
         'plone.app.dexterity',
         'collective.folderishtypes[dexterity]',
-        'dexterity.membrane',
+        'dexterity.membrane>=3.0.0a1',
+        'Products.membrane>=5.0.0a1',
     ],
     extras_require={
         'test': [
