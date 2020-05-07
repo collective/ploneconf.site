6eedc0fb0c50d8d5c17a035a00c22525c912c7dc
Philip Bauer <bauer@starzel.de>
2020-05-07 14:16:46 +0200

    some small schema changes

--- src/ploneconf/site/content/speaker.py
+++ src/ploneconf/site/content/speaker.py
@@ -1,27 +1,65 @@
 # -*- coding: utf-8 -*-
 from dexterity.membrane.behavior.user import DxUserObject
-from dexterity.membrane.content.member import IMember
+from dexterity.membrane.content.member import IEmail
 from plone.app.textfield import RichText
 # from plone.autoform import directives
-from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
+from plone.dexterity.content import Item
+from plone.namedfile import field as namedfile
+from plone.schema.email import Email
 # from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 from ploneconf.site import _
 from Products.membrane.interfaces import IMembraneUserRoles
 # from z3c.form.browser.radio import RadioFieldWidget
 from zope.component import adapter
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-class ISpeaker(IMember):
+class ISpeaker(IEmail):
     """ Marker interface and Dexterity Python Schema for Speaker
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('speaker.xml')
+    first_name = schema.TextLine(
+        title=_(u'First Name'),
+        required=True,
+    )
+
+    last_name = schema.TextLine(
+        title=_(u'Last Name'),
+        required=True,
+    )
+
+    company = schema.TextLine(
+        title=_(u'Company'),
+        required=False,
+        )
+
+    job_title = schema.TextLine(
+        title=_(u'Job Title'),
+        required=False,
+        )
+
+    website = schema.TextLine(
+        title=_(u'Website'),
+        required=False,
+        )
+
+    twitter = schema.TextLine(
+        title=_(u'Twitter name'),
+        required=False,
+        )
+
+    github = schema.TextLine(
+        title=_(u'Github username'),
+        required=False,
+        )
+
+    image = namedfile.NamedBlobImage(
+        title=_(u'Image'),
+        description=_(u'Portrait of the speaker'),
+        required=False,
+        )
 
     speaker_biography = RichText(
         title=_(u'Speaker Biography (max. 1000 characters)'),
@@ -29,44 +67,9 @@ class ISpeaker(IMember):
         required=False,
         )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
 
 @implementer(ISpeaker)
-class Speaker(Container):
+class Speaker(Item):
     """
     """
 
