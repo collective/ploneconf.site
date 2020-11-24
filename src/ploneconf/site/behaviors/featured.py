c8054a98a17feb0dcbb1a986f5d753fa4f13995e
Philip Bauer <bauer@starzel.de>
2020-11-24 19:36:47 +0100

    remove annoying fieldset from behavior

--- src/ploneconf/site/behaviors/featured.py
+++ src/ploneconf/site/behaviors/featured.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.supermodel import directives
 from plone.supermodel import model
 from zope import schema
 from zope.interface import provider
@@ -8,12 +7,6 @@ from zope.interface import provider
 
 @provider(IFormFieldProvider)
 class IFeatured(model.Schema):
-
-    directives.fieldset(
-        'featured',
-        label=u'Featured',
-        fields=('featured',),
-    )
 
     featured = schema.Bool(
         title=u'Show this item on the frontpage',
