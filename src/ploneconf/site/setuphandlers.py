c9fcbfe806fc83c1a5e69f927d7bf7788b80b639
Philip Bauer <bauer@starzel.de>
2020-12-03 18:52:54 +0100

    Add hacks to get Volto to play well with Plone 5.2:] - folderish types - editable frontpage by adding blocks and blocks_layout as properties - install restapi - block-behavior for Documents

--- src/ploneconf/site/setuphandlers.py
+++ src/ploneconf/site/setuphandlers.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+
+import json
+
+
+HOMEPAGE = {
+    "title": "Plone Conference 2035!",
+    "description": "The digital experience conference",
+    "blocks": {
+        "15068807-cfc9-444a-97db-8c736809ff51": {"@type": "title"},
+        "59d41d8a-ef05-4e21-8820-2a64f5878098": {
+            "@type": "text",
+            "text": {
+                "blocks": [
+                    {
+                        "key": "618bl",
+                        "text": "Plone is a CMS built on Python with over 19 years of experience. Plone has a plethora of features that appeal to developers and users alike, such as customizable content types, hierarchical URL object traversing and a sophisticated content workflow powered by a granular permissions model. This allows you to build anything from simple websites to enterprise-grade intranets. Volto exposes all these features and communicates with Plone via its mature REST API. Volto can be esily themed and is highly customizable.",
+                        "type": "unstyled",
+                        "depth": 0,
+                        "inlineStyleRanges": [],
+                        "entityRanges": [],
+                        "data": {},
+                    }
+                ],
+                "entityMap": {},
+            },
+        },
+    },
+    "blocks_layout": {
+        "items": [
+            "15068807-cfc9-444a-97db-8c736809ff51",
+            "59d41d8a-ef05-4e21-8820-2a64f5878098",
+        ]
+    },
+}
 
 
 @implementer(INonInstallable)
@@ -16,6 +51,24 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    create_volto_homepage()
+
+
+def create_volto_homepage(default_home=HOMEPAGE):
+    portal = api.portal.get()
+    blocks = default_home['blocks']
+    blocks_layout = default_home['blocks_layout']
+
+    portal.setTitle(default_home['title'])
+    portal.setDescription(default_home['description'])
+
+    if not getattr(portal, 'blocks', False):
+        portal.manage_addProperty('blocks', json.dumps(blocks), 'string')
+
+    if not getattr(portal, 'blocks_layout', False):
+        portal.manage_addProperty(
+            'blocks_layout', json.dumps(blocks_layout), 'string'
+        )
 
 
 def uninstall(context):
