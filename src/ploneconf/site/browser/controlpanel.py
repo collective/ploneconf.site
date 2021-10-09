b25ba2d608d347415ae053d3eb85ce9384691571
ksuess <k.suess@rohberg.ch>
2021-10-09 15:56:41 +0200

    Footureproof vocabulary: Register select options for choice field as dictionary
    … with token/value and title, instead of registering list of texts.
    
    No upgrade step for ControlPanel fields as this is training code, free of any history.

--- src/ploneconf/site/browser/controlpanel.py
+++ src/ploneconf/site/browser/controlpanel.py
@@ -1,13 +1,12 @@
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.restapi.controlpanels import RegistryConfigletPanel
-from plone.z3cform import layout
 from zope import schema
 from zope.component import adapter
 from zope.interface import Interface
 
 
-class IPloneconfControlPanel(Interface):
+class IPloneconfSettings(Interface):
 
     talk_submission_open = schema.Bool(
         title="Allow talk submission",
@@ -16,47 +15,66 @@ class IPloneconfControlPanel(Interface):
         required=False,
     )
 
-    types_of_talk = schema.List(
-        title="Available types for talks",
-        default=["Talk", "Training", "Keynote", "Lightning Talk"],
-        missing_value=None,
+    types_of_talk = schema.Dict(
+        title="Types of Talk",
+        description="Available types of a talk",
+        default={
+            "talk": "Talk",
+            "training": "Training",
+            "keynote": "Keynote",
+            "lightning-talk": "Lightning Talk",
+        },
+        missing_value={},
         required=False,
+        key_type=schema.TextLine(),
         value_type=schema.TextLine(),
     )
 
-    audiences = schema.List(
-        title="Available audiences for talks",
-        default=["Beginner", "Advanced", "Professional"],
-        missing_value=None,
+    audiences = schema.Dict(
+        title="Audience",
+        description="Available audiences of a talk",
+        default={
+            "beginner": "Beginner",
+            "advanced": "Advanced",
+            "professional": "Professional",
+        },
+        missing_value={},
         required=False,
+        key_type=schema.TextLine(),
         value_type=schema.TextLine(),
     )
 
-    rooms = schema.Tuple(
-        title="Available Rooms for the conference",
-        default=("101", "201", "Auditorium"),
-        missing_value=None,
+    rooms = schema.Dict(
+        title="Rooms",
+        description="Available rooms of the conference",
+        default={
+            "101": "101",
+            "201": "201",
+            "auditorium": "Auditorium",
+        },
+        missing_value={},
         required=False,
+        key_type=schema.TextLine(),
         value_type=schema.TextLine(),
     )
+
+
+class PloneconfRegistryEditForm(RegistryEditForm):
+    schema = IPloneconfSettings
+    schema_prefix = 'ploneconf'
+    label = 'Ploneconf Settings'
+
+
+class PloneConfControlPanelFormWrapper(ControlPanelFormWrapper):
+    form = PloneconfRegistryEditForm
 
 
 @adapter(Interface, Interface)
-class PloneconfControlPanel(RegistryConfigletPanel):
-    schema = IPloneconfControlPanel
-    schema_prefix = "ploneconf"
-    configlet_id = "ploneconf-controlpanel"
-    configlet_category_id = "General"
-    title = "Ploneconf Settings"
-    group = "Products"
-
-
-class PloneconfControlPanelForm(RegistryEditForm):
-    schema = IPloneconfControlPanel
-    schema_prefix = "ploneconf"
-    label = "Ploneconf Settings"
-
-
-PloneconfControlPanelView = layout.wrap_form(
-    PloneconfControlPanelForm, ControlPanelFormWrapper
-)
+class PloneConfRegistryConfigletPanel(RegistryConfigletPanel):
+    """Volto control panel"""
+    schema = IPloneconfSettings
+    schema_prefix = 'ploneconf'
+    configlet_id = 'ploneconf-controlpanel'
+    configlet_category_id = 'Products'
+    title = 'Ploneconf Settings'
+    group = 'Products'
