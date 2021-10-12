5b19b3579acaefc021a877d1c3d390586773ebf0
ksuess <k.suess@rohberg.ch>
2021-10-12 14:34:07 +0200

    flake8

--- src/ploneconf/site/content/talk.py
+++ src/ploneconf/site/content/talk.py
@@ -10,8 +10,6 @@ from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
 
 
 class ITalk(model.Schema):
