aa7786621b4e993569ef36c0b8c6a868bc9a6b26
Katja Süss <k.suess@rohberg.ch>
2024-09-10 15:04:20 +0200

    Add content type talk

---
+++ src/ploneconf/site/content/talk.py
@@ -0,0 +1,86 @@
+from plone.app.textfield import RichText
+from plone.autoform import directives
+from plone.dexterity.content import Container
+from plone.namedfile.field import NamedBlobImage
+from plone.schema.email import Email
+from plone.supermodel import model
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from z3c.form.browser.radio import RadioFieldWidget
+from zope import schema
+from zope.interface import implementer
+
+
+class ITalk(model.Schema):
+    """Dexterity-Schema for Talks"""
+
+    directives.widget(type_of_talk=RadioFieldWidget)
+    type_of_talk = schema.Choice(
+        title="Type of talk",
+        values=["talk", "training", "keynote"],
+        required=True,
+    )
+
+    details = RichText(
+        title="Details",
+        description="Description of the talk (max. 2000 characters)",
+        max_length=2000,
+        required=True,
+    )
+
+    directives.widget(audience=CheckBoxFieldWidget)
+    audience = schema.Set(
+        title="Audience",
+        value_type=schema.Choice(
+            values=["beginner", "advanced", "professional"],
+        ),
+        required=False,
+    )
+
+    speaker = schema.TextLine(
+        title="Speaker",
+        description="Name (or names) of the speaker",
+        required=False,
+    )
+
+    company = schema.TextLine(
+        title="Company",
+        required=False,
+    )
+
+    email = Email(
+        title="Email",
+        description="Email address of the speaker",
+        required=False,
+    )
+
+    website = schema.TextLine(
+        title="Website",
+        required=False,
+    )
+
+    twitter = schema.TextLine(
+        title="Twitter name",
+        required=False,
+    )
+
+    github = schema.TextLine(
+        title="Github username",
+        required=False,
+    )
+
+    image = NamedBlobImage(
+        title="Image",
+        description="Portrait of the speaker",
+        required=False,
+    )
+
+    speaker_biography = RichText(
+        title="Speaker Biography (max. 1000 characters)",
+        max_length=1000,
+        required=False,
+    )
+
+
+@implementer(ITalk)
+class Talk(Container):
+    """Talk instance class"""
